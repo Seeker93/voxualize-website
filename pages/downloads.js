c05a5fa219756ad775440a62238207091662358d
Shuaib Parker <parkershuaib@gmail.com>
2020-10-20 18:54:44 +0200

    More changes

--- pages/downloads.js
+++ pages/downloads.js
@@ -2,13 +2,12 @@ export default function Downloads() {
     return (
         <div className="container mt-5">
             <h1>Downloads</h1>
-            <p>
-                Our project documents can be found below:
-            </p>
-            <button className="btn btn-primary mr-5 mt-3 mb-3"><a className="text-white" href="CARTA-volumetric-rendering-PRKSHU001-WDMJON001-revised.pdf">Proposal</a></button><br/>
-            <button className="btn btn-primary mt-3 mb-3"><a className="text-white" href="prkshu001/prkshu001-final-project-paper-carta.pdf"> Shuaib's paper</a></button><br/>
-            <button className="btn btn-primary mt-3"><a className="text-white" href="prkshu001/prkshu001-final-project-paper-carta.pdf"> Jonathans's paper</a></button>
-
+            <p>Our project documents can be found below:</p>
+            <div className="row justify-content-space-between mt-5">
+            <button className="btn btn-primary mr-5"><a className="text-white" href="CARTA-volumetric-rendering-PRKSHU001-WDMJON001-revised.pdf">Proposal</a></button>
+            <button className="btn btn-primary mr-5"><a className="text-white" href="prkshu001/prkshu001-final-project-paper-carta.pdf"> Shuaib's paper</a></button>
+            <button className="btn btn-primary "><a className="text-white" href="prkshu001/prkshu001-final-project-paper-carta.pdf"> Jonathans's paper</a></button>
+            </div>
         </div>
     )
 
--- pages/team.js
+++ pages/team.js
@@ -7,15 +7,18 @@ export default function Team() {
       <div className="container row justify-content-space-between ">
         <h5 className="description mb-5 col">
           Jonathan Weideman (WDMJON001)
+
           </h5>
 
         <h5 className="description mb-5 col">
           Shuaib Parker (PRKSHU001) <br/>
-
           <img src="shuaib.jpeg" height="200px" className="mt-5"/>
           </h5> 
       
       </div>
+      <h3 className="description mb-5 col">Work allocation</h3>
+      <p>Shuaib was responsible for the client, and Jonathan was responsible for the server. See <a href="/client">client</a> and <a href="/server">server</a> for more details.</p>
+  
 
     </div>
 
